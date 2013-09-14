7d64397b3464f7f112b1e1163d1c0fe4e625c3be
William Stein <williamstein92@gmail.com>
2013-09-14 15:27:03 -0400

    FIX: altered loading symbol.

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,26 @@
 (function (window) {
     $('.sorry, .loading, .particle-wrapper').toggleClass('visible invisible');
 
-    var height          = 450,
+    var height          = 800,
         orbits          = $('.particle-orbit'),
         particle_size   = [[11, 13],[13, 20],[12, 21],[7, 14],[7, 12],[10, 11],[12, 14],[7, 6],[13, 15],[9, 12],[11, 12],[16, 16],[6, 7],[9, 14],[12, 17]],
-        size_factor     = [1,.8,.9,.5,1,1,.85,.7,.6,.65,.8,.55,.9,.98,.68,.7],
-        angle           = [20,-20,50,60,50,-40,30,40,70,-70,20,20,-30,30,-40],
-        delay           = [-1,1,.5,-.5,0,-1,0,1,.5,-.5,0,-.25,0,.25,0,.75];
+        size_factor     = [1,.8,.9,.5,.58,.72,.85,.7,.6,.65,.8,.55,.9,.98,.68,.4],
+        angle           = [15,-20,50,60,55,-45,30,40,70,-70,25,20,-30,35,-40],
+        delay           = [-1,1,.5,-.5,0,-1,0,1,.5,-.5,0,-.25,0,.25,0,.75],
+        counter         = false;
+    $('.particle-orbit').on('mouseover touchstart', function () {
+        if (counter === false) {
+            $('.particle-collection').css('animation', 'screenRotate 1s linear infinite alternate');
+            counter = true;
+        }
+    });
+
+    $('.particle-orbit').on('mouseout touchend', function () {
+        if (counter === true) {
+            $('.particle-collection').css('animation', 'none');
+            counter = false;
+        }
+    });
 
     for (var i = 0; i < orbits.length; i++) {
         var size = height/5 * size_factor[i];
@@ -33,4 +47,34 @@
 
         ellipsis.html(str);
     }, 750)
+
+    var T = function (n) {
+        return 4*C(n) + 8*S(n) + 4*M(n) + 4*D(n) + 4*Z(n) + O(n);
+        },
+        C = function (n) {
+            if (n < 2) return 1;
+            return 2*S(n-1) + 3*D(n-1) + 4*Z(n-1);
+        },
+        S = function (n) {
+            if (n < 2) return 1;
+            return 4*S(n-1) + 3*D(n-1) + O(n-1) + 2*Z(n-1) + C(n-1) + 2*M(n-1);
+        },
+        M = function (n) {
+            if (n < 2) return 1;
+            return 6*S(n-1) + 4*D(n-1) + 3*Z(n-1);
+        }, 
+        D = function (n) {
+            if (n < 2) return 1;
+            return 6*S(n-1) + O(n-1) + 4*Z(n-1) + 3*C(n-1) + 4*M(n-1);
+        }, 
+        Z = function (n) {
+            if (n < 2) return 1;
+            return 4*S(n-1) + 4*D(n-1) + O(n-1) + 2*Z(n-1) + 4*C(n-1) + 3*M(n-1);
+        }, 
+        O = function (n) {
+            if (n < 2) return 1;
+            return 8*S(n-1) + 4*D(n-1) + 4*Z(n-1);
+        }
+
+    window.T = T;
 })(window)
